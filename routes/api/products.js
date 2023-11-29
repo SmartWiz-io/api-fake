abd985dde9b6eaa1e6e63e9a554661a82c23f83c
Geoffrey DeFilippi <gdefilippi@smartwiz.io>
2023-11-28 22:27:12 -0800

    (feat):return json

--- routes/api/products.js
+++ routes/api/products.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 // GET all products
-router.get("/getUploadUrl", (req, res) => {
+router.get("/getUploadUrl", async (req, res) => {
 	const reqparams = req.query
 	const incoming = req.body; 
-	const fakedata = `https://upload.aws.s3.something.com\?bid=${reqparams.businessId}&uid=${reqparams.userId}`;
-	res.json(JSON.stringify(fakedata));
+	const fakedata = `{"bid":"${reqparams.businessId}","uid":"${reqparams.userId}"}`;
+	res.json(fakedata);
 });
 
 
